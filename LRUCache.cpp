41e2164f69f0a61820531065630878b28847d360
Ananya-garg2007 <ananya24.garg@gmail.com>
2025-12-26 18:20:19 +0530

    Update LRUCache.cpp

---
+++ LRUCache.cpp
@@ -0,0 +1,55 @@
+#include "LRUCache.h"
+
+LRUCache::LRUCache(int c) {
+    cap = c;
+}
+
+bool LRUCache::has(string key) {
+    return mp.find(key) != mp.end();
+}
+
+LU LRUCache::get(string key) {
+   
+    if (mp.find(key) == mp.end()) {
+        cout << key << endl;
+        LU empty;
+        return empty;
+    }
+
+    
+    auto it = mp.find(key);
+    order.erase(it->second.second);
+    order.push_front(key);
+    it->second.second = order.begin();
+
+    return it->second.first;
+}
+
+void LRUCache::put(string key, LU val) {
+    
+    if (mp.find(key) != mp.end()) {
+        auto it = mp.find(key);
+        order.erase(it->second.second);
+        order.push_front(key);
+        it->second = make_pair(val, order.begin());
+        return;
+    }
+
+    if ((int)mp.size() >= cap) {
+        string old = order.back();
+        order.pop_back();
+        mp.erase(old);
+
+        cout << "Cache full. Removing oldest key: " << old << endl;
+    }
+
+  
+    order.push_front(key);
+    mp[key] = make_pair(val, order.begin());
+}
+
+void LRUCache::clear() {
+    order.clear();
+    mp.clear();
+    
+}
